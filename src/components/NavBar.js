88a7d11f15ad4e720fa249ff2aa2188816d17e0b
ashleydelic <ashleycohzer96@gmail.com>
2022-12-03 20:49:57 -0500

    google fonts import

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
+import {GiSkills, } from 'react-icons/fa';
 
 function NavBar() {
   return (
     <div>
-        <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-500 text-white shadow-lg'>
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
+     <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-500 text-white shadow-lg'>
+      
     </div>
     </div>
   )
 }
+
+
 
 export default NavBar
