988c632ea2a4484824098227ea9342ecae7edf0c
ashleydelic <ashleycohzer96@gmail.com>
2022-12-04 17:50:02 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,9 @@ const Home = () => {
   return (
     <div name='home' className=' h-screen w-full'>
      <div className=' max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full px-4 md:flex-row'>
-         <div>
+         <div className='flex flex-col justify-center h-full'>
              <h2>Hello! I'm Ashley 👋🏽.</h2>
-             <p>As a self-taught front-end developer, my joy comes from building robust, intuitive and user-friendly interfaces.</p>
+             <p>As a self-taught front-end developer, my joy comes from building fun, intuitive and user-friendly interfaces.</p>
          </div>
          <div>
              <button>
