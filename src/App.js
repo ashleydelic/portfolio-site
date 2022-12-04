4399c509387153e1c657daee049b32e6a4a9c798
ashleydelic <ashleycohzer96@gmail.com>
2022-12-03 20:18:03 -0500

    tailwind installations and tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1>Hey Milan!</h1>
+    <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-500 text-white shadow-lg'>
+      <i>A</i>
+      <i>B</i>
+      <i>C</i>
+      <i>D</i>
     </div>
   )
 }
 
-export default App;
+export default App
