43c1dfa6f54a272731ebe59e5e01da86f8e4ae21
ashleydelic <ashleycohzer96@gmail.com>
2022-12-04 14:07:02 -0500

    react icons import

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import HeroImage from '../assets/portrait.jpeg';
+import {BiArrowFromLeft, BiArrowFromTop} from 'react-icons/bi';
 
 const Home = () => {
   return (
     <div>
-      
+      <img src={HeroImage} alt='' />
     </div>
   )
 }
