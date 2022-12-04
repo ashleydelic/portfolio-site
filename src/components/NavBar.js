6fc23bd583f4a4cce19c6ad3191a8fae3fc0e262
ashleydelic <ashleycohzer96@gmail.com>
2022-12-03 21:25:03 -0500

    navbar background

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,8 @@ import {FaThList, FaRegWindowClose} from 'react-icons/fa';
 function NavBar() {
   return (
     <div>
-     <div className='flex justify-between items-center w-full h-20 text-white fixed'>
-         <FaThList />
+     <div className='flex justify-between items-center w-full h-20 text-white bg-orange-400 px-4 fixed'>
+         <h1 className='text-4xl font-signature'>Ashley</h1>
       
     </div>
     </div>
