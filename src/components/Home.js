b956de9dfeb6068096236362d6a6b5d98a76efde
ashleydelic <ashleycohzer96@gmail.com>
2022-12-04 18:58:37 -0500

    h1 font fire animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <div name='home' className=' h-screen w-full'>
      <div className=' max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full px-4 md:flex-row'>
          <div className='flex flex-col justify-center h-full'>
-             <h2>Hello! I'm Ashley 👋🏽.</h2>
+             <h1 className='text-4xl sm:text-4xl font-bold font-effect-fire-animation'>Hello! I'm Ashley 👋🏽.</h1>
              <p>As a self-taught front-end developer, my joy comes from building fun, intuitive and user-friendly interfaces.</p>
          </div>
          <div>
