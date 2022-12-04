e8f6d6fcb3a6fd26424f6f4052a2e9c0cb65f806
ashleydelic <ashleycohzer96@gmail.com>
2022-12-04 16:24:10 -0500

    intro paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,12 @@ import {BiArrowFromLeft, BiArrowFromTop} from 'react-icons/bi';
 const Home = () => {
   return (
     <div>
-      <img src={HeroImage} alt='' />
+     <div>
+         <div>
+             <h2>Hello! I'm Ashley.</h2>
+             <p>A software developer with a passion for front end development. I enjoy building robust, intuitive and user-friendly interfaces.</p>
+         </div>
+     </div>
     </div>
   )
 }
