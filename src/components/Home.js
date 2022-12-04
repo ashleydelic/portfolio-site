b22c177d07fd25c3847f735c0fdc27c25358366a
ashleydelic <ashleycohzer96@gmail.com>
2022-12-04 17:41:56 -0500

    h2 update with more styling on the entire page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,22 @@ import {BiArrowFromLeft, BiArrowFromTop} from 'react-icons/bi';
 
 const Home = () => {
   return (
-    <div>
-     <div>
+    <div name='home' className=' h-screen w-full'>
+     <div className=' max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full px-4 md:flex-row'>
          <div>
-             <h2>Hello! I'm Ashley.</h2>
-             <p>As a self-taught software developer, my joy comes from building robust, intuitive and user-friendly interfaces.</p>
+             <h2>Hello! I'm Ashley 👋🏽.</h2>
+             <p>As a self-taught front-end developer, my joy comes from building robust, intuitive and user-friendly interfaces.</p>
          </div>
          <div>
              <button>
-                 Portfolio <BiArrowFromLeft />
+                 Portfolio 
+                 <span>
+                 <BiArrowFromLeft />
+                 </span>
              </button>
+         </div>
+         <div>
+             <img src={HeroImage} alt='my illustration' className=' rounded-2xl mx-auto w-2/3 md:w-full' />
          </div>
      </div>
     </div>
