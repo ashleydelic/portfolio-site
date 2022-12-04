1e3fdc8ad161a76d307b4601e33bcc43b16c1082
ashleydelic <ashleycohzer96@gmail.com>
2022-12-04 10:44:05 -0500

    first element with tailwind styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {FaThList, FaRegWindowClose} from 'react-icons/fa';
 
-function NavBar() {
+const NavBar = () => {
   return (
-    <div>
-     <div className='flex justify-between items-center w-full h-20 text-white bg-orange-400 px-4 fixed'>
-         <h1 className='text-4xl font-signature'>Ashley</h1>
-      
+    <div className='flex justify-between items-center w-full h-20 text-white bg-orange-400 px-4 fixed'>
+     <div>
+         <h1 className='text-4xl font-signature ml-3'>Ashley</h1>
     </div>
+
+    <ul className='flex'>
+       <li className='px-4 cursor-pointer capitalize font-normal text-white hover:scale-110 duration-200'>Home</li>
+
+    </ul>
     </div>
   )
 }
