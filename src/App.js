165225b53b2c84f96a33bb184cb0d0c9bf5c6029
ashleydelic <ashleycohzer96@gmail.com>
2022-12-03 20:20:30 -0500

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-500 text-white shadow-lg'>
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
-    </div>
+  <div>
+    <NavBar />
+  </div>
   )
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-500 text-white shadow-lg'>
+      <i>A</i>
+      <i>B</i>
+      <i>C</i>
+      <i>D</i>
+    </div>
+    </div>
+  )
+}
+
+export default NavBar
