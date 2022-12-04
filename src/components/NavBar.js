b5f24e1c1d5b39a7158ebc216398e3ecc4569439
ashleydelic <ashleycohzer96@gmail.com>
2022-12-04 12:50:05 -0500

    name h1 delete, p tag instead

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import { FaThList, FaRegWindowClose } from "react-icons/fa";
 
+
 const NavBar = () => {
+
     const [nav, setNav] = useState(false);
+
   return (
+    
     <div className="flex justify-between items-center w-full h-20 text-white bg-orange-400 px-4 fixed">
-      <div>
-        <h1 className="text-4xl font-signature ml-3 cursor-pointer hover:scale-105 duration-200">
-          Ashley
-        </h1>
+    <div>
+      <div className="text-2xl font-signature ml-3 cursor-pointer hover:scale-105 duration-200">
+        <p className="px-3">Ashley</p>
+        <p className=" text-white rounded bg-green-700 px-4">Delić</p>
       </div>
+    </div>
 
       <ul className="md:flex hidden mt-2 md:text-[22px] tracking-widest">
         <li className="px-4 cursor-pointer text-white hover:scale-110 duration-200 font-normal">
@@ -26,9 +31,11 @@ const NavBar = () => {
         </li>
       </ul>
 
-      <div onClick={() => setNav(!nav)} className="cursor-pointer pr-3 z-10 text-green-700">
+      <div onClick={() => setNav(!nav)} className="cursor-pointer pr-3 z-10 text-green-800">
          {nav ? <FaRegWindowClose size={20} /> : <FaThList size={20}/>}
       </div>
+
+      <ul className="flex"></ul>
     </div>
   );
 };
