d63a58ea45b383dc41ff6a67f7865972f657bd38
ashleydelic <ashleycohzer96@gmail.com>
2022-12-04 21:35:37 -0500

    sidebar links update

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -7,10 +7,31 @@ const SocialLinks = () => {
   return (
     <div className='flex flex-col top-[35%] left-0 fixed'>
       <ul>
-          <li className='flex justify-between items-center w-40 h-1/4 px-4 bg-slate-400 text-white'>
+          <li className='flex justify-between items-center w-40 h-14 rounded-t-md px-4 bg-slate-400 text-white ml-[-100px] hover:ml-[10px] hover:rounded-md duration-200'>
+              <a href="https://www.linkedin.com/in/ashleydelic/" className='flex justify-between items-center w-full'>
+              <>
+             LinkedIn <FaLinkedin size={30}/>
+              </>
+              </a>
+          </li>
+          <li className='flex justify-between items-center w-40 h-14 px-4 bg-slate-400 text-white ml-[-100px] hover:ml-[10px] hover:rounded-md duration-200'>
+              <a href="https://github.com/ashleydelic" className='flex justify-between items-center w-full'>
+              <>
+             GitHub <FaGithub size={30}/>
+              </>
+              </a>
+          </li>
+          <li className='flex justify-between items-center w-40 h-14 px-4 bg-slate-400 text-white ml-[-100px] hover:ml-[10px] hover:rounded-md duration-200'>
+              <a href="mailto:ashleycohzer96@gmail.com" className='flex justify-between items-center w-full'>
+              <>
+             Email <HiOutlineMailOpen size={30}/>
+              </>
+              </a>
+          </li>
+          <li className='flex justify-between items-center w-40 h-14 px-4 bg-slate-400 rounded-b-md text-white ml-[-100px] hover:ml-[10px] hover:rounded-md duration-200'>
               <a href="" className='flex justify-between items-center w-full'>
               <>
-             LinkedIn <FaLinkedin />
+             Resume <BsFilePerson size={30}/>
               </>
               </a>
           </li>
