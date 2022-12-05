a7663b3d536200640b8ac94c7bd4a9cf1b9fb911
ashleydelic <ashleycohzer96@gmail.com>
2022-12-05 11:48:09 -0500

    about section paragraphs

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div>
       <div>
-          <div>
-              <p></p>
-          </div>
+        <div>
+          <p>About Me</p>
+        </div>
+
+        <p>
+          Excited is an understatement! 😄 Hi again, I'm Ashley Delić, a
+          Front-end developer based out of Kentucky, USA but born and raised in
+          South Africa . From traveling the world as a cruise ship spa therapist
+          ⛴, to finding my passion in writing self-taught lines of code 👩🏾‍💻.{" "}
+          <br></br>
+        </p>
+
+        <p>
+          I am proficient in HTML, CSS, and JavaScript, as well as the React framework, still learning others as well. I have
+          been implementing my knowledge and keeping track of my personal projects by storing them with Git and GitHub. <br></br>
+          I am thrilled to take the leap and kickstart my career and learn among professionals
+        </p>
+
+        <br></br>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
