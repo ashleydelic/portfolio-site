b5c4628899b4885deb18e729c7140710fafbd847
ashleydelic <ashleycohzer96@gmail.com>
2022-12-05 17:43:51 -0500

    typescript effect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import HeroImage from '../assets/portrait.jpeg';
+import Typewriter from 'typewriter-effect';
 
+const string = [
+  "Hello! I'm Ashley"
+  
+];
 
 const Home = () => {
   return (
     <div name='home' className=' h-screen w-full'>
      <div className=' max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full px-4 md:flex-row'>
          <div className='flex flex-col justify-center h-full'>
-             <h1 className='text-4xl sm:text-4xl font-bold font-effect-fire-animation'>Hello! I'm Ashley 👋🏽.</h1>
-             <p className=' py-3 text-lg'>As a front-end developer, my joy ignites from building and seeing fun, intuitive and user-friendly interfaces come to life!</p>
+             <h1 className='text-4xl sm:text-4xl font-bold'>{" "} <Typewriter
+                  options={{ strings: string, autoStart: true, loop: true }} 
+                /> 👋🏽 </h1>
+             <p className=' py-3 text-lg'>As a front-end developer, my joy ignites from building and seeing fun, intuitive and user-friendly interfaces come to life!
+             </p>
          </div>
          <div>
              
