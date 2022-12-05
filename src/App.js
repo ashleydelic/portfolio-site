37b4b55e853595df4f075400c7280797d4759247
ashleydelic <ashleycohzer96@gmail.com>
2022-12-05 17:56:37 -0500

    projects component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import NavBar from './components/NavBar'
 import Home from './components/Home'
 import SocialLinks from './components/SocialLinks'
 import About from './components/About'
-
+import Projects
+ from './components/Projects'
 function App() {
   return (
   <div>
     <NavBar />
     <Home />
     <About />
+    <Projects />
 
     <SocialLinks />
   </div>
