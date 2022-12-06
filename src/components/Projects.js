96468fbd24111c20f3fb24bf6b6bb7d6265b5beb
ashleydelic <ashleycohzer96@gmail.com>
2022-12-05 20:28:37 -0500

    project img

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import WeatherApp from '../assets/weather-app.png';
+import CounterApp from '../assets/counter-app.png';
+import PortfolioApp from '../assets/portfolio-app.png';
+import QuoteApp from '../assets/quote-app.png';
 
 const Projects = () => {
   return (
     <div>
-      
+      <div>
+          <div>
+              <p>Projects</p>
+          </div>
+      </div>
     </div>
   )
 }
