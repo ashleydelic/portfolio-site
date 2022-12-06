45486324fe853858d0395430ec0e7310af0cc669
ashleydelic <ashleycohzer96@gmail.com>
2022-12-05 20:36:43 -0500

    project skeleton

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import WeatherApp from '../assets/weather-app.png';
 import CounterApp from '../assets/counter-app.png';
 import PortfolioApp from '../assets/portfolio-app.png';
 import QuoteApp from '../assets/quote-app.png';
+import BlackJackApp from '../assets/blackjack-app.png';
 
 const Projects = () => {
   return (
@@ -10,6 +11,18 @@ const Projects = () => {
       <div>
           <div>
               <p>Projects</p>
+              <p>Check out some of my projects:</p>
+          </div>
+
+          <div>
+              <div>
+                  <img src={} alt="" />
+                  <div>
+                      <button>View live</button>
+                      <button>Codebase</button>
+                  </div>
+                  
+              </div>
           </div>
       </div>
     </div>
