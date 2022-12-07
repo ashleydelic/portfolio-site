4b5f6bd888bdbfd3fa3eb65fd4d5251e8fe97dd8
ashleydelic <ashleycohzer96@gmail.com>
2022-12-06 20:17:26 -0500

    name and email input

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div id='contact'>
+    <div id="contact">
       <div>
-          <div>
-              <p>Let's connect! 🤝</p>
-              <p>Submit form below to get in touch </p>
-          </div>
+        <div>
+          <p>Let's connect! 🤝</p>
+          <p>Submit form below to get in touch </p>
+        </div>
+
+        <div>
+          <form action="">
+            <input
+              type=""
+              name="name"
+              placeholder="Enter your name"
+              className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+            />
+            <input
+              type=""
+              name="email"
+              placeholder="Email"
+              className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+            />
+          </form>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
